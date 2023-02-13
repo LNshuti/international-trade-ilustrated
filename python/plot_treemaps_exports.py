71c9c4b9bffad5182da5c71f6b782fd44f7d59de
LNshuti <nshutl0@sewanee.edu>
2023-02-12 20:59:59 -0800

    add usa plot

--- python/plot_treemaps_exports.py
+++ python/plot_treemaps_exports.py
@@ -55,3 +55,17 @@ plt.savefig('../output/usa_exports_treemap.png', bbox_inches='tight')
 print(usa_df.head(10))
 
 
+# Filter to the top 10 products by trade balance for CHN 
+rus_df = labelled_df[labelled_df['location_code'] == 'RUS'].sort_values(by='trade_balance', ascending=False).head(10)
+
+plt.figure(figsize=(8,6))
+# Plot the treemap
+squarify.plot(sizes=rus_df['trade_balance'], label=rus_df['parent_code'], alpha=.8 )
+# Remove the axis
+plt.axis('off')
+# Save the plot as a png file
+plt.savefig('../output/russia_exports_treemap.png', bbox_inches='tight')
+
+print(rus_df.head(10))
+
+
