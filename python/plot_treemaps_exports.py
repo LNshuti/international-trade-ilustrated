638c05440d2dc0de902412aeef47712c69a5c513
Leonce Nshuti <LNshuti@users.noreply.github.com>
2023-02-12 21:10:27 -0600

    minor updates

--- python/plot_treemaps_exports.py
+++ python/plot_treemaps_exports.py
@@ -17,15 +17,16 @@ trade_data_all_years['import_value'] = pd.to_numeric(trade_data_all_years['impor
 trade_data_all_years['trade_balance'] = trade_data_all_years['export_value'] - trade_data_all_years['import_value']
 
 labelled_df = trade_data_all_years.merge(product_labs, left_on='sitc_product_code', right_on='parent_code', how='inner')
-
+print(labelled_df.head(10))
 # Filter location_code to the USA, CHN, and RUS (China, Russia, and the United States)
 labelled_df = labelled_df[labelled_df['location_code'].isin(['USA', 'CHN', 'RUS'])]
 
 # Summarize the trade_balance by location_code and product_description
-labelled_df = labelled_df.groupby(['location_code', 'description'])['trade_balance'].sum().reset_index()
+labelled_df = labelled_df.groupby(['location_code', 'parent_code'])['trade_balance'].sum().reset_index()
 
 # Filter to the top 10 products by trade balance for CHN 
 labelled_df = labelled_df[labelled_df['location_code'] == 'CHN']
+print(labelled_df.head(10))
 china_df = labelled_df.sort_values(by='trade_balance', ascending=False).head(10)
 
 
@@ -34,11 +35,10 @@ china_df = labelled_df.sort_values(by='trade_balance', ascending=False).head(10)
 plt.figure(figsize=(10,10))
 
 # Plot the treemap
-squarify.plot(sizes=china_df['trade_balance'], label=china_df['description'], alpha=.8 )
+squarify.plot(sizes=china_df['trade_balance'], label=china_df['parent_code'], alpha=.8 )
 
 # Remove the axis
 plt.axis('off')
-
 
 # Save the plot as a png file
 plt.savefig('../output/china_exports_treemap.png', bbox_inches='tight')
