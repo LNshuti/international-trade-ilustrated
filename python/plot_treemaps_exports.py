5f7f3bc04504f724b95d6e4ba0d3cb586f0dc0b9
Leonce Nshuti <LNshuti@users.noreply.github.com>
2023-02-13 16:49:04 -0600

    update china exports plot

--- python/plot_treemaps_exports.py
+++ python/plot_treemaps_exports.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 # Read in the data
 product_labs = pd.read_csv('../data/processed/SITCCodeandDescription.csv')
-print(product_labs.head())
 
 # Read in the data
 trade_data_all_years = pq.ParquetDataset('../data/country_partner_sitcproduct4digit_year_2020.parquet').read_pandas().to_pandas()
@@ -17,7 +16,7 @@ trade_data_all_years['import_value'] = pd.to_numeric(trade_data_all_years['impor
 trade_data_all_years['trade_balance'] = trade_data_all_years['export_value'] - trade_data_all_years['import_value']
 
 labelled_df = trade_data_all_years.merge(product_labs, left_on='sitc_product_code', right_on='parent_code', how='inner')
-print(labelled_df.head(10))
+#print(labelled_df.head(10))
 # Filter location_code to the USA, CHN, and RUS (China, Russia, and the United States)
 labelled_df = labelled_df[labelled_df['location_code'].isin(['USA', 'CHN', 'RUS'])]
 
@@ -29,17 +28,17 @@ china_df = labelled_df[labelled_df['location_code'] == 'CHN'].sort_values(by='tr
 
 # Using squarify to plot treemaps
 # Save plot as png file
-plt.figure(figsize=(8,6))
-# Plot the treemap
-squarify.plot(sizes=china_df['trade_balance'], label=china_df['parent_code'], alpha=.8 )
-# Remove the axis
-plt.axis('off')
-# Save the plot as a png file
-plt.savefig('../output/china_exports_treemap.png', bbox_inches='tight')
+# plt.figure(figsize=(8,6))
+# # Plot the treemap
+# squarify.plot(sizes=china_df['trade_balance'], label=china_df['parent_code'], alpha=.8 )
+# # Remove the axis
+# plt.axis('off')
+# # Save the plot as a png file
+# plt.savefig('../output/china_exports_treemap.png', bbox_inches='tight')
 
 # Select location_code and description
-#labelled_df = labelled_df[['description', 'parent_code']]
-print(china_df.head(10))
+china_labelled_df = china_df['location_code']
+print(china_labelled_df)
 
 # # Filter to the top 10 products by trade balance for CHN 
 # usa_df = labelled_df[labelled_df['location_code'] == 'USA'].sort_values(by='trade_balance', ascending=False).head(10)
