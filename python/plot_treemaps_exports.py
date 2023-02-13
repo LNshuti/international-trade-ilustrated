c256d37ee52c2402e0d1e08ab406c60a0577246d
Leonce Nshuti <LNshuti@users.noreply.github.com>
2023-02-12 20:38:41 -0600

    update python code

--- python/plot_treemaps_exports.py
+++ python/plot_treemaps_exports.py
@@ -1,5 +1,6 @@
 # Use python to plot treemaps of exports by country and by product
 
+import pyarrow.parquet as pq
 import pandas as pd
 import squarify
 import matplotlib.pyplot as plt
@@ -8,4 +9,6 @@ import matplotlib.pyplot as plt
 product_labs = pd.read_csv('../data/processed/SITCCodeandDescription.csv')
 print(product_labs.head())
 
-
+# Read in the data
+trade_data_all_years = pq.ParquetDataset('../data/trade_data_all_years.parquet').read_pandas().to_pandas()
+print(trade_data_all_years.head())
