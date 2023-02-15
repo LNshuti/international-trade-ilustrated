543a2c51b684eb967e6f15a6ade88b6520195940
Leonce Nshuti <LNshuti@users.noreply.github.com>
2023-02-14 22:01:44 -0600

    add polars loop to append files

--- python/explore_tradebalance_overtime.py
+++ python/explore_tradebalance_overtime.py
@@ -4,14 +4,18 @@ import polars as pl
 def load_data(filename):
     return pl.read_parquet(filename) 
 
+# Create variable that holds the path to the data
+data_path = 'D:\portfolio\dataverse-files\parquet'
+
 # Write the main function 
 def main():
     # Load the data 
     # Iterate through all years and load parquet files from 2000 to 2020 
     trade_data_all_years = pl.DataFrame()
-    for year in range(2020, 2021):
+    for year in range(2019, 2021):
         # Load the data
-        trade_data = load_data(f'../data/country_partner_sitcproduct4digit_year_{year}.parquet')
+        trade_data = load_data(f'{data_path}/country_partner_sitcproduct4digit_year_{year}.parquet')
+        #trade_data = load_data(f'../data/country_partner_sitcproduct4digit_year_{year}.parquet')
         # Convert the export_value and import_value columns to numeric
         trade_data = trade_data.with_columns([
             pl.col('export_value').cast(pl.Float64),
@@ -20,9 +24,10 @@ def main():
             pl.col('year').cast(pl.Int32),
         ])
 
-        trade_data_all_years = trade_data
+        print(trade_data.schema)
+        #trade_data_all_years = pl.concat(trade_data)
 
-    print(trade_data_all_years)
+    #print(trade_data_all_years)
 
 # Call the main function
 if __name__ == '__main__':
