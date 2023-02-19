7dfb94017d400f6871d606d25ec111d00114a046
Leonce Nshuti <LNshuti@users.noreply.github.com>
2023-02-19 08:15:18 -0600

    update generation code

--- python/explore_energy_generation.py
+++ python/explore_energy_generation.py
@@ -19,25 +19,20 @@ trade_data_all_years['trade_balance'] = trade_data_all_years['export_value'] - t
 labelled_df = trade_data_all_years.merge(product_labs, left_on='sitc_product_code', right_on='parent_code', how='inner')
 #print(labelled_df.head(10))
 # Filter location_code to the USA, CHN, and RUS (China, Russia, and the United States)
-labelled_df = labelled_df[labelled_df['location_code'].isin(['USA', 'CHN', 'RUS'])]
+labelled_df = labelled_df[labelled_df['location_code'].isin(['RWA', 'BDI', 'UGA'])]
+#print(labelled_df.head(10))
 
 # Summarize the trade_balance by location_code and product_description
-labelled_df = labelled_df.groupby(['location_code', 'parent_code', 'description'])['trade_balance'].sum().reset_index()
+labelled_df = labelled_df.groupby(['location_code', 'parent_code', 'partner_code', 'description'])['trade_balance'].sum().reset_index()
 
 # Filter to the top 10 products by trade balance for CHN 
-usa_df = labelled_df[labelled_df['location_code'] == 'RUS'].sort_values(by='trade_balance', ascending=False)
+usa_df = labelled_df[labelled_df['location_code'] == 'RWA'].sort_values(by='trade_balance', ascending=False)
 
 # Select unique values from the parent_code and description columns 
-usa_df = usa_df[['parent_code', 'description']].drop_duplicates()
+usa_df = usa_df[['parent_code','location_code', 'partner_code', 'description', 'trade_balance']].drop_duplicates()
 
 # convert to polars dataframe
 usa_df = pl.from_pandas(usa_df)
 print(usa_df)
 # usa_df = usa_df[usa_df['parent_code'].isin(['0342'])]
 
-
-# print(usa_df.reset_index(drop=True))
-
-predicate = pl.col("description").str.contains("gold")
-
-print(usa_df[predicate])
