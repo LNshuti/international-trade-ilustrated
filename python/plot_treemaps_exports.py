e91ea1252a77565c3826c6925251c2e4f934ede4
LNshuti <nshutl0@sewanee.edu>
2023-02-20 12:36:35 -0800

    update readme

--- python/plot_treemaps_exports.py
+++ python/plot_treemaps_exports.py
@@ -376,7 +376,7 @@ def main():
         plt.xlabel('Trade Balance Per Capita in USD')
         plt.ylabel('')
         # Seaborn decreasethe font size of y labels 
-        plt.yticks(color='purple')
+        plt.yticks('')
         plt.savefig('../output/top10partners_all_countries_df_' + location_code + '.png', dpi=900)
 
     # Call the function
