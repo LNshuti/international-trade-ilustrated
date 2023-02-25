99758d9f56fd4141f271af288a6eef3f35843cde
LNshuti <nshutl0@sewanee.edu>
2023-02-25 12:14:51 -0800

    update rwa analysis

--- python/explore_energy_generation.py
+++ python/explore_energy_generation.py
@@ -38,14 +38,27 @@ rwa_df = rwa_df.groupby(['location_code', 'partner_code', 'description'])['trade
 # Create a new variable trade_balance_millions to make the numbers more readable
 rwa_df['trade_balance_millions'] = rwa_df['trade_balance'] / 1000000
 
-
 # Filter to the top 10 products by trade balance for CHN 
 top10 = rwa_df.sort_values(by='trade_balance', ascending=False).head(20)
- 
+  
+# Import the population data  
+pop_data = pd.read_csv('../data/processed/API_SP_POP_TOTL_DS2.csv', skiprows=4)
+
+# Merge the population data to the trade data
+pop_data = pop_data[['Country Code', 'Country Name', '2020']]
+
+# Rename 2020 to pop_2020
+pop_data = pop_data.rename(columns={'2020': 'pop_2020'})
+
+# Join pop_data 
+rwa_df = top10.merge(pop_data, left_on='partner_code', right_on='Country Code', how='inner')
+
+print(rwa_df.head(10))
+
 # Make the font human readable 
 sns.set(font_scale=0.8)
 sns.set_style("whitegrid")
-sns.catplot(x='trade_balance_millions', y='partner_code', data=top10, palette='Blues_d', kind='bar', height=6, aspect=0.8)
+sns.catplot(x='trade_balance_millions', y='Country Name', data=rwa_df, palette='Blues_d', kind='bar', height=6, aspect=0.8)
 plt.title('')
 plt.xlabel('Trade Balance in Million USD')
 plt.ylabel('')
@@ -53,7 +66,15 @@ plt.ylabel('')
 plt.yticks(fontsize=8, color='grey')
 #plt.show()
 plt.savefig('../output/rwanda_top_trade_partners' + '.png', dpi=300, bbox_inches='tight')
-# rwa_df = rwa_df[rwa_df['parent_code'].isin(['0342'])]
+
+# Plot bar plot andsave plot as png to output folder. Use seaborn for styling
+# fig, ax = plt.subplots(figsize=(5, 3))
+# sns.set_style("whitegrid")
+# sns.catplot(x='trade_balance_millions', y='partner_code', data=df.to_pandas(), palette='Blues_d', kind='bar')
+# plt.title("Country Name")
+# plt.xlabel('Trade Balance In Millions of USD')
+# plt.ylabel('')
+# plt.savefig('../output/top10partners_' + location_code + '.png', dpi=300, bbox_inches='tight')
 
 #Use python do download the data about what percentage of energy source is solar, versus coal, and other renewables and non renewables. I want data for the following countries: China, Russia, USA, Brazil, India, South Africa, and the ASEAN countries. Please also add Germany, France, and the UK.
 #Write python code below
