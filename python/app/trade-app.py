823cd237bfe7f11c44d6454b682bdba86dff6e86
LNshuti <nshutl0@sewanee.edu>
2023-02-28 19:22:37 -0800

    update streamlit

--- python/app/trade-app.py
+++ python/app/trade-app.py
@@ -30,25 +30,41 @@ def main():
         
         return labelled_df
     
+    data = load_data()
     # Implement selector for state 
     location_code = st.sidebar.selectbox('Select location_code', data['location_code'].unique())
 
     # Implement selector for partner_code 
     partner_code = st.sidebar.selectbox('Select partner_code', data['partner_code'].unique())
+
+    # Implement selector for description 
+    description = st.sidebar.selectbox('Select description', data['description'].unique())
     
     data = data[data['location_code'] == location_code]
     data = data[data['partner_code'] == partner_code]
+    data = data[data['description'] == description]
+
+    # Find the top 10 export_value by location_code and partner_code
+    data = data.sort_values(by='export_value', ascending=False).head(10)
 
     st.write(data)
     # Plot a histogram of the total award 
-    # Write code below 
-    # st.write('''Total Government Contract Issuange by Agency''')
-    # fig, ax = plt.subplots()
-    # ax.hist(data['Total Award($)'], bins=20)
-    # ax.set_xlabel('Total Award($Millions)')
-    # ax.set_ylabel('Frequency')
-    # ax.set_title('Total Award Distribution')
-    # st.pyplot(fig)
+     
+    # Select distinct location_code and use it in the title 
+    location_code = data['location_code'].unique()
+    partner_code = data['partner_code'].unique()
+
+    # Append location_code to the title
+    st.title('''Imports by ''' + str(location_code[0]) + ''' from ''' + str(partner_code[0]) + ''' in 2020''')
+
+    # Calculate sum of import_value by location_code and partner_code
+    data = data.groupby(['location_code', 'partner_code', 'sitc_product_code', 'description']).sum().reset_index()
+
+    fig, ax = plt.subplots()
+    ax.hist(data['import_value'])
+    ax.set_xlabel('Imports ($Millions)')
+    ax.set_ylabel('Frequency')
+    st.pyplot(fig)
 
     if st.sidebar.checkbox("Show raw data", False):
         st.subheader('Raw data')
