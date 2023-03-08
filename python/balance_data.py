26f0675caba9dfcccf67547c47b925aba8481775
LNshuti <nshutl0@sewanee.edu>
2023-03-08 14:00:49 -0800

    add copied code from opeanai and facebook research. Test local execution

---
+++ python/balance_data.py
@@ -0,0 +1,18 @@
+from balance import load_data, Sample
+
+def main():
+    # load simulated example data
+    target_df, sample_df = load_data()
+
+    # Import sample and target data into a Sample object
+    sample = Sample.from_frame(sample_df, outcome_columns=["happiness"])
+    target = Sample.from_frame(target_df)
+
+    # Set the target to be the target of sample
+    sample_with_target = sample.set_target(target)
+
+    # Check basic diagnostics of sample vs target before adjusting:
+    # sample_with_target.covars().plot()
+
+if __name__ == '__main__':
+    main()
