3bef8e4419d499d3a45da10ebc3672ec27d7f5d5
Leonce Nshuti <LNshuti@users.noreply.github.com>
2023-03-13 10:40:48 -0500

    update remote

--- python/explore_tradebalance_overtime.py
+++ python/explore_tradebalance_overtime.py
@@ -1,4 +1,8 @@
 import polars as pl
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 # Define function that loads data in parquet format 
 def load_data(filename):
@@ -25,13 +29,26 @@ def main():
         ])
 
         trade_data_subset = trade_data.select(
-            ['year', 'location_code','partner_code', 'sitc_product_code', 'sitc_eci', 'sitc_coi', 'export_value', 'import_value']
+            ['export_value','year', 'location_code','partner_code', 'sitc_product_code', 'sitc_eci', 'sitc_coi',  'import_value']
         )
         
         trade_data_all_years = pl.concat([trade_data], how='vertical')
 
     print(trade_data_all_years)
     trade_data_all_years.write_parquet(f'../data/processed/country_partner_sitcproduct4digit_year_all.parquet')
+
+
+
+    Y=trade_data_all_years.iloc[:,0]
+    X=trade_data_all_years.iloc[:, 1:6]
+    ##TRANSFORMING Y:
+    Y[Y >=0.5] = 1
+    Y[Y <0.5] = 0
+    x_train, x_test, y_train, y_test = train_test_split(trade_data_all_years.iloc[:, 1:6], trade_data_all_years.iloc[:,0],test_size = 0.25)
+    x_train=x_train.replace([np.inf, -np.inf], 9999).dropna(axis=0)
+    x_test=x_test.replace([np.inf, -np.inf], 9999).dropna(axis=0)
+
+    print(x_train)
 # Call the main function
 if __name__ == '__main__':
     main()
